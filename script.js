29d6e5336a5082f8e2dcb10ec8dc163fcceb4da8
Ayaz Dyshin <ayazdyshin@gmail.com>
2022-09-29 21:00:28 +0200

    basic setup

---
+++ script.js
@@ -0,0 +1,14 @@
+const bod = document.querySelector('.container');
+for (let i = 1; i <= 16; i++) {
+    let newDiv = document.createElement('div');
+    newDiv.classList.add('row');
+    bod.appendChild(newDiv);
+    for (let i = 1; i <= 16; i++) {
+        let cellDiv = document.createElement('div');
+        cellDiv.classList.add('cell');
+        cellDiv.textContent = 'testing';
+        newDiv.appendChild(cellDiv);
+    }
+
+
+}
