e1fb8f64e69ce48e9f87213e9d31237d4af3af18
Ayaz Dyshin <ayazdyshin@gmail.com>
2022-10-02 17:04:34 +0200

    add main logic, add size picker, add clear button

--- script.js
+++ script.js
@@ -1,12 +1,54 @@
 const bod = document.querySelector('.container');
-for (let i = 1; i <= 16; i++) {
-    let newDiv = document.createElement('div');
-    newDiv.classList.add('row');
-    bod.appendChild(newDiv);
-    for (let i = 1; i <= 16; i++) {
-        let cellDiv = document.createElement('div');
-        cellDiv.classList.add('cell');
-        cellDiv.style.backgroundColor = 'black';
-        newDiv.appendChild(cellDiv);
+const sizeButton = document.querySelector('.changeSize');
+let currentSize = 16;
+const handleSizeButton = () => {
+    let newSize = prompt("choose a size, max value is 100");
+    clearGrid();
+    setUpGrid(newSize);
+    currentSize = newSize;
+}
+sizeButton.addEventListener('click', handleSizeButton);
+let mouseDown = false;
+document.body.onmousedown = () => (mouseDown = true);
+document.body.onmouseup = () => (mouseDown = false);
+
+
+
+const colorOnClick = (event) => {
+    event.preventDefault();
+    if (event.type === 'mouseover' && !mouseDown) return
+    event.currentTarget.style.backgroundColor = 'black';
+}
+
+const changeSize = (size) => {
+    clearGrid();
+}
+
+
+const clearGrid = () => {
+    bod.innerHTML = '';
+}
+
+const resetGrid = () => {
+    clearGrid();
+    setUpGrid(currentSize);
+}
+const clearButton = document.querySelector('.clearButton');
+clearButton.addEventListener('click', resetGrid);
+const setUpGrid = (size) => {
+    for (let i = 1; i <= size; i++) {
+        let newDiv = document.createElement('div');
+        newDiv.classList.add('row');
+        bod.appendChild(newDiv);
+
+        for (let i = 1; i <= size; i++) {
+            let cellDiv = document.createElement('div');
+            cellDiv.classList.add('cell');
+            cellDiv.addEventListener('mouseover', colorOnClick);
+            cellDiv.addEventListener('mousedown', colorOnClick);
+            newDiv.appendChild(cellDiv);
+        }
     }
 }
+
+setUpGrid(16);
